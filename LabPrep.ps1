6de120d299e4457bc9d6138b6fb29156db2242fc
olivarezsj <48802777+olivarezsj@users.noreply.github.com>
2020-01-07 13:18:10 -0700

    Update LabPrep.ps1

--- LabPrep.ps1
+++ LabPrep.ps1
@@ -16,6 +16,7 @@ if($deploymentType -eq "Lab"){
     $domain = Get-ADDomain
     $domainFQDN = $domain.dnsroot
     $domusername = "$($domainFQDN)\$($username)"
+    New-ADOrganizationalUnit Clients -Path $domain.DistinguishedName
     New-ADOrganizationalUnit Servers -Path $domain.DistinguishedName
     $ServerOU = "ou=Servers,$($domain.DistinguishedName)"
     New-ADOrganizationalUnit PKI -Path $ServerOU
@@ -39,6 +40,62 @@ if($deploymentType -eq "Lab"){
     Set-Item WSMan:\localhost\Client\TrustedHosts -Value $option3 -Force
     Invoke-Command -ComputerName $option3 -Credential $creds -ScriptBlock $scriptblockcontent -ArgumentList ($domainFQDN,$domusername,$password,$option3,$APPOU)
     Remove-Item WSMan:\localhost\Client\TrustedHosts -Include $option3
+    
+    $CAInstcreds = New-Object System.Management.Automation.PSCredential($domusername, $securePassword)
+    $CAInstallscriptblockcontent = {
+        Param ($Arg1,$Arg2,$Arg3)
+        $CAName = "$Arg1-RootCA"
+        $CAcred = New-Object -Typename System.Management.Automation.PSCredential -Argumentlist ($Arg2), (ConvertTo-SecureString $Arg3 -asplaintext -force)
+        Install-AdcsCertificationAuthority -CAType EnterpriseRootCa -CACommonName $CAName -Credential $CAcred -CryptoProviderName "ECDSA_P256#Microsoft Software Key Storage Provider" -KeyLength 256 -HashAlgorithmName SHA256
+        }
+    Invoke-Command -ComputerName $option1 -Credential $CAInstcreds -ScriptBlock $CAInstallscriptblockcontent -ArgumentList ($deploymentType,$username,$password)
+    
+    #Create new Certificate Template
+    $WebServerCT = Get-ADObject -Identity "CN=WebServer,CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,$($domain.DistinguishedName)" -Properties *
+    $CertTemplateAttributes = @{
+        "flags"="131649";
+        "revision"="100";
+        "pKIDefaultKeySpec" = "1";
+        "pKIMaxIssuingDepth"="0";
+        "pKICriticalExtensions"="2.5.29.15";
+        "pKIExtendedKeyUsage"="1.3.6.1.5.5.7.3.1"
+        "pKIDefaultCSPs"=@("1,Microsoft RSA SChannel Cryptographic Provider","2,Microsoft DH SChannel Cryptographic Provider");
+        "msPKI-RA-Signature"="0";
+        "msPKI-Enrollment-Flag"="0";
+        "msPKI-Private-Key-Flag"="101056768";
+        "msPKI-Certificate-Name-Flag"="1";
+        "msPKI-Minimal-Key-Size"="2048"
+        "msPKI-Template-Schema-Version"="4";
+        "msPKI-Template-Minor-Revision"="2";
+        "msPKI-Cert-Template-OID"="1.3.6.1.4.1.311.21.8.15767579.15718837.13781421.6810505.13041814.86.16015666.16460173";
+        "msPKI-Certificate-Application-Policy"="1.3.6.1.5.5.7.3.1";
+        "pKIKeyUsage"=$WebServerCT.pKIKeyUsage;
+        "pKIExpirationPeriod"=$WebServerCT.pKIExpirationPeriod;
+        "pKIOverlapPeriod"=$WebServerCT.pKIOverlapPeriod
+    }   
+    New-ADObject -Name UpdatedWebServer -Type pKICertificateTemplate -DisplayName UpdatedWebServer -OtherAttributes $CertTemplateAttributes `
+    -Path "CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,$($domain.DistinguishedName)"
+
+    #Modify permissions on new certificate template
+    [System.GUID]$autoenrollGuid = (Get-ADObject -Identity "CN=Certificate-AutoEnrollment,CN=Extended-Rights,CN=Configuration,$($domain.DistinguishedName)" -Properties rightsGuid).rightsGuid
+    [System.GUID]$enrollGuid = (Get-ADObject -Identity "CN=Certificate-Enrollment,CN=Extended-Rights,CN=Configuration,$($domain.DistinguishedName)" -Properties rightsGuid).rightsGuid
+    $certificateTemplate = Get-ADObject -Identity "CN=UpdatedWebServer,CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,DC=resource,DC=lab"
+    $APP1_SID = New-Object System.Security.Principal.SecurityIdentifier (Get-ADComputer $option2).SID
+    $APP2_SID = New-Object System.Security.Principal.SecurityIdentifier (Get-ADComputer $option3).SID
+    $CertTempAcl = Get-ACL -Path ("AD:$($certificateTemplate.DistinguishedName)")
+    $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
+    $APP1_SID,"ReadProperty, GenericExecute","Allow")) 
+    $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
+    $APP1_SID,"ExtendedRight","Allow",$autoenrollGuid))   
+    $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
+    $APP1_SID,"ExtendedRight","Allow",$enrollGuid)) 
+    $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
+    $APP2_SID,"ReadProperty, GenericExecute","Allow")) 
+    $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
+    $APP2_SID,"ExtendedRight","Allow",$autoenrollGuid))   
+    $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
+    $APP2_SID,"ExtendedRight","Allow",$enrollGuid)) 
+    Set-ACL -ACLObject $acl -Path ("AD:$($ct.DistinguishedName)")
 }
 if($deploymentType -eq "Workstation"){
     [array]$splitDom = $option1.split(".")
@@ -50,62 +107,4 @@ if($deploymentType -eq "Workstation"){
 if($deploymentType -eq "PKI"){
     Import-module servermanager
     Install-WindowsFeature -Name Adcs-Cert-Authority -IncludeManagementTools
-}
-if($deploymentType -eq "DeployEntCA"){
-        #Installs CA Services
-        $CAInstcreds = New-Object System.Management.Automation.PSCredential($domusername, $securePassword)
-        $CAInstallscriptblockcontent = {
-            Param ($Arg1,$Arg2,$Arg3)
-            $CAName = "$Arg1-RootCA"
-            $CAcred = New-Object -Typename System.Management.Automation.PSCredential -Argumentlist ($Arg2), (ConvertTo-SecureString $Arg3 -asplaintext -force)
-            Install-AdcsCertificationAuthority -CAType EnterpriseRootCa -CACommonName $CAName -Credential $CAcred -CryptoProviderName "ECDSA_P256#Microsoft Software Key Storage Provider" -KeyLength 256 -HashAlgorithmName SHA256
-            }
-        Invoke-Command -ComputerName $option1 -Credential $CAInstcreds -ScriptBlock $CAInstallscriptblockcontent -ArgumentList ($deploymentType,$username,$password)
-        
-        #Create new Certificate Template
-        $WebServerCT = Get-ADObject -Identity "CN=WebServer,CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,$($domain.DistinguishedName)" -Properties *
-        $CertTemplateAttributes = @{
-            "flags"="131649";
-            "revision"="100";
-            "pKIDefaultKeySpec" = "1";
-            "pKIMaxIssuingDepth"="0";
-            "pKICriticalExtensions"="2.5.29.15";
-            "pKIExtendedKeyUsage"="1.3.6.1.5.5.7.3.1"
-            "pKIDefaultCSPs"=@("1,Microsoft RSA SChannel Cryptographic Provider","2,Microsoft DH SChannel Cryptographic Provider");
-            "msPKI-RA-Signature"="0";
-            "msPKI-Enrollment-Flag"="0";
-            "msPKI-Private-Key-Flag"="101056768";
-            "msPKI-Certificate-Name-Flag"="1";
-            "msPKI-Minimal-Key-Size"="2048"
-            "msPKI-Template-Schema-Version"="4";
-            "msPKI-Template-Minor-Revision"="2";
-            "msPKI-Cert-Template-OID"="1.3.6.1.4.1.311.21.8.15767579.15718837.13781421.6810505.13041814.86.16015666.16460173";
-            "msPKI-Certificate-Application-Policy"="1.3.6.1.5.5.7.3.1";
-            "pKIKeyUsage"=$WebServerCT.pKIKeyUsage;
-            "pKIExpirationPeriod"=$WebServerCT.pKIExpirationPeriod;
-            "pKIOverlapPeriod"=$WebServerCT.pKIOverlapPeriod
-        }   
-        New-ADObject -Name UpdatedWebServer -Type pKICertificateTemplate -DisplayName UpdatedWebServer -OtherAttributes $CertTemplateAttributes `
-        -Path "CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,$($domain.DistinguishedName)"
-    
-        #Modify permissions on new certificate template
-        [System.GUID]$autoenrollGuid = (Get-ADObject -Identity "CN=Certificate-AutoEnrollment,CN=Extended-Rights,CN=Configuration,$($domain.DistinguishedName)" -Properties rightsGuid).rightsGuid
-        [System.GUID]$enrollGuid = (Get-ADObject -Identity "CN=Certificate-Enrollment,CN=Extended-Rights,CN=Configuration,$($domain.DistinguishedName)" -Properties rightsGuid).rightsGuid
-        $certificateTemplate = Get-ADObject -Identity "CN=UpdatedWebServer,CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,DC=resource,DC=lab"
-        $APP1_SID = New-Object System.Security.Principal.SecurityIdentifier (Get-ADComputer $option2).SID
-        $APP2_SID = New-Object System.Security.Principal.SecurityIdentifier (Get-ADComputer $option3).SID
-        $CertTempAcl = Get-ACL -Path ("AD:$($certificateTemplate.DistinguishedName)")
-        $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
-        $APP1_SID,"ReadProperty, GenericExecute","Allow")) 
-        $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
-        $APP1_SID,"ExtendedRight","Allow",$autoenrollGuid))   
-        $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
-        $APP1_SID,"ExtendedRight","Allow",$enrollGuid)) 
-        $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
-        $APP2_SID,"ReadProperty, GenericExecute","Allow")) 
-        $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
-        $APP2_SID,"ExtendedRight","Allow",$autoenrollGuid))   
-        $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
-        $APP2_SID,"ExtendedRight","Allow",$enrollGuid)) 
-        Set-ACL -ACLObject $acl -Path ("AD:$($ct.DistinguishedName)")
 }
