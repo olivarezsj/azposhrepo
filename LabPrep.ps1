9597edce2b9215edb1a2357c1e2f927b05555d4d
olivarezsj <48802777+olivarezsj@users.noreply.github.com>
2020-01-08 10:48:34 -0700

    Update LabPrep.ps1

--- LabPrep.ps1
+++ LabPrep.ps1
@@ -68,32 +68,37 @@ if($deploymentType -eq "Lab"){
     Invoke-Command -ComputerName $option1 -Credential $CAInstcreds -ScriptBlock $CAInstallscriptblockcontent -ArgumentList ($deploymentType,$domusername,$password)
     
     #Create new Certificate Template
-    $WebServerCT = Get-ADObject -Identity "CN=WebServer,CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,$($domain.DistinguishedName)" -Properties *
-    $CertTemplateAttributes = @{
-        "flags"="131649";
-        "revision"="100";
-        "pKIDefaultKeySpec" = "1";
-        "pKIMaxIssuingDepth"="0";
-        "pKICriticalExtensions"="2.5.29.15";
-        "pKIExtendedKeyUsage"="1.3.6.1.5.5.7.3.1"
-        "pKIDefaultCSPs"=@("1,Microsoft RSA SChannel Cryptographic Provider","2,Microsoft DH SChannel Cryptographic Provider");
-        "msPKI-RA-Signature"="0";
-        "msPKI-Enrollment-Flag"="0";
-        "msPKI-Private-Key-Flag"="101056768";
-        "msPKI-Certificate-Name-Flag"="1";
-        "msPKI-Minimal-Key-Size"="2048"
-        "msPKI-Template-Schema-Version"="4";
-        "msPKI-Template-Minor-Revision"="2";
-        "msPKI-Cert-Template-OID"="1.3.6.1.4.1.311.21.8.15767579.15718837.13781421.6810505.13041814.86.16015666.16460173";
-        "msPKI-Certificate-Application-Policy"="1.3.6.1.5.5.7.3.1";
-        "pKIKeyUsage"=$WebServerCT.pKIKeyUsage;
-        "pKIExpirationPeriod"=$WebServerCT.pKIExpirationPeriod;
-        "pKIOverlapPeriod"=$WebServerCT.pKIOverlapPeriod
-    }   
-    New-ADObject -Name UpdatedWebServer -Type pKICertificateTemplate -DisplayName UpdatedWebServer -OtherAttributes $CertTemplateAttributes `
-    -Path "CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,$($domain.DistinguishedName)"
-
-    #Modify permissions on new certificate template
+    try {
+        $WebServerCT = Get-ADObject -Identity "CN=WebServer,CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,$($domain.DistinguishedName)" -Properties *
+        $CertTemplateAttributes = @{
+            "flags"="131649";
+            "revision"="100";
+            "pKIDefaultKeySpec" = "1";
+            "pKIMaxIssuingDepth"="0";
+            "pKICriticalExtensions"="2.5.29.15";
+            "pKIExtendedKeyUsage"="1.3.6.1.5.5.7.3.1"
+            "pKIDefaultCSPs"=@("1,Microsoft RSA SChannel Cryptographic Provider","2,Microsoft DH SChannel Cryptographic Provider");
+            "msPKI-RA-Signature"="0";
+            "msPKI-Enrollment-Flag"="0";
+            "msPKI-Private-Key-Flag"="101056768";
+            "msPKI-Certificate-Name-Flag"="1";
+            "msPKI-Minimal-Key-Size"="2048"
+            "msPKI-Template-Schema-Version"="4";
+            "msPKI-Template-Minor-Revision"="2";
+            "msPKI-Cert-Template-OID"="1.3.6.1.4.1.311.21.8.15767579.15718837.13781421.6810505.13041814.86.16015666.16460173";
+            "msPKI-Certificate-Application-Policy"="1.3.6.1.5.5.7.3.1";
+            "pKIKeyUsage"=$WebServerCT.pKIKeyUsage;
+            "pKIExpirationPeriod"=$WebServerCT.pKIExpirationPeriod;
+            "pKIOverlapPeriod"=$WebServerCT.pKIOverlapPeriod
+        }   
+        New-ADObject -Name UpdatedWebServer -Type pKICertificateTemplate -DisplayName UpdatedWebServer -OtherAttributes $CertTemplateAttributes `
+        -Path "CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,$($domain.DistinguishedName)"
+    }
+    catch {
+        Write-Output $_.Exception.Message
+    }
+    try {
+            #Modify permissions on new certificate template
     [System.GUID]$autoenrollGuid = (Get-ADObject -Identity "CN=Certificate-AutoEnrollment,CN=Extended-Rights,CN=Configuration,$($domain.DistinguishedName)" -Properties rightsGuid).rightsGuid
     [System.GUID]$enrollGuid = (Get-ADObject -Identity "CN=Certificate-Enrollment,CN=Extended-Rights,CN=Configuration,$($domain.DistinguishedName)" -Properties rightsGuid).rightsGuid
     $certificateTemplate = Get-ADObject -Identity "CN=UpdatedWebServer,CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,DC=resource,DC=lab"
@@ -113,6 +118,13 @@ if($deploymentType -eq "Lab"){
     $CertTempAcl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
     $APP2_SID,"ExtendedRight","Allow",$enrollGuid)) 
     Set-ACL -ACLObject $acl -Path ("AD:$($ct.DistinguishedName)")
+    }
+    catch {
+        Write-Output $_.Exception.Message
+    }
+
+
+
 }
 if($deploymentType -eq "Workstation"){
     [array]$splitDom = $option1.split(".")
